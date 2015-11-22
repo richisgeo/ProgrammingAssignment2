8606efd6d8aa691ecd808ab3aed158bde0d03dbd
richisgeo <richisgeo@gmail.com>
2015-11-22 18:21:19 -0500

    Programming Assignment 2 complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+library(tm)
+text <- readLines("res.txt", encoding="UTF-8")
+txtsrc <- VectorSource(text)
+corpus <- Corpus(txtsrc)
+corpus <- tm_map(corpus, content_transformer(tolower))
 
-## Write a short comment describing this function
+corpus <- tm_map(corpus, removePunctuation)
 
-makeCacheMatrix <- function(x = matrix()) {
+corpus <- tm_map(corpus, stripWhitespace)
 
-}
+corpus <- tm_map(corpus, removeWords, stopwords("english"))
+dtm <- DocumentTermMatrix(corpus)
+dtm2 <- as.matrix(dtm)
+frequency <- colSums(dtm2)
+frequency <- sort(frequency, decreasing=TRUE)
 
+head(frequency)
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
