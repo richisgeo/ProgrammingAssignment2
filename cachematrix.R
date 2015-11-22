466972b68c8b5e348127e881ac708ea1ddf039a0
richisgeo <richisgeo@gmail.com>
2015-11-22 18:27:07 -0500

    prog 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,49 @@
-library(tm)
-text <- readLines("res.txt", encoding="UTF-8")
-txtsrc <- VectorSource(text)
-corpus <- Corpus(txtsrc)
-corpus <- tm_map(corpus, content_transformer(tolower))
+##makeCacheMatrix creats a matrix, and saves it to memory. it also contains
+## a list of functions (set,get,getmatrix and setmatrix)
 
-corpus <- tm_map(corpus, removePunctuation)
+makeCacheMatrix <- function(x = matrix()) {
+  #creates a null m
+  m<-NULL
+  set<-function(y){
+    
+    x<<-y
+    m<<-NULL
+  }
+  
+  
+  get<-function() x
+  
+  ##s runs solve on matrix if it is invertible for the sake of this project
+  ## no tests were run on the matrix to see if it's invertible or not.
+  setmatrix<-function(solve) m<<- solve
+  
+  #returns matrix
+  getmatrix<-function() m
+  
+  # a list of functions set,get, setmatrix, get matrix
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
+}
 
-corpus <- tm_map(corpus, stripWhitespace)
 
-corpus <- tm_map(corpus, removeWords, stopwords("english"))
-dtm <- DocumentTermMatrix(corpus)
-dtm2 <- as.matrix(dtm)
-frequency <- colSums(dtm2)
-frequency <- sort(frequency, decreasing=TRUE)
+## cacheSolve returns the inverse of the matrix that is in the cache memory
 
-head(frequency)
-
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  # gets the matrix using the get function in makecachematrix
+  matrix<-x$get()
+  # solves it...by returning its inverse to 'm'
+  
+  
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
+  
+  
+}
